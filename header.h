f800387345f6d59b27ed18d75fd8cd3e93aa436e
jurgisad <144427804+jurgisad@users.noreply.github.com>
2023-09-27 23:35:40 +0300

    Update header.h

--- header.h
+++ header.h
@@ -10,13 +10,14 @@
 #include <algorithm>
 #include <cstdlib> // For rand() and srand() functions
 #include <ctime>   // For time() function
+#include <fstream>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
 using std::vector;
-
+using std::ifstream;
 using std::setw;
 using std::fixed;
 using std::left;
@@ -25,12 +26,17 @@ using std::setprecision;
 
 int randint(int nuo, int iki);
 
+double skaiciuotiGalutini(Studentas& studentas);
+double skaiciuotiMediana(Studentas& studentas);
+
 struct Studentas{
     string Vardas, Pavarde;
     vector <int> nd;
     int egz;
     double galutinis, mediana;
 };
+
+void nuskaitytiIsFailo(vector<Studentas>& studentai);
 
 void ivestis(vector<Studentas>& studentai);
 
