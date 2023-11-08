fd58b1545bf99cbad6d51271af9c1533a8d1f19b
jurgisad <144427804+jurgisad@users.noreply.github.com>
2023-11-08 18:44:12 +0200

    Update main.cpp
    Padaryta, kad vel butu galima ivesti ranka, parasytas for ciklas testavimui su vektoriais.

--- main.cpp
+++ main.cpp
@@ -2,157 +2,70 @@
 #include "timer.h"
 
 int main() {
-    vector<Studentas> studentai1, studentai2, studentai3, studentai4, studentai5;
-    vector<Studentas> studentai1_bahurai, studentai2_bahurai, studentai3_bahurai, studentai4_bahurai, studentai5_bahurai;
-    vector<Studentas> studentai1_slabakai, studentai2_slabakai, studentai3_slabakai, studentai4_slabakai, studentai5_slabakai;
-
+    vector<vector<Studentas>> studentai(5);
+    vector<vector<Studentas>> bahurai(5);
+    vector<vector<Studentas>> slabakai(5);
+    vector<Studentas> studentai_ivedimas;
     char choice;
+    char choice2;
+
+
     cout << "Norite naudoti mediana ar vidurki ? (Mediana - a, Vidurkis - b): ";
     ived_tikr_a_or_b(choice);
+    cout << "Norite naudoti ivedima ranka ar generuoti failus ? (ranka - a, generuoti - b): ";
+    ived_tikr_a_or_b(choice2);
+    if (choice2=='a')
+    {
+        ivestis(studentai_ivedimas);
 
-    generavimas(1000, 5, "stud_1000.txt");
-    generavimas(10000, 5, "stud_10000.txt");
-    generavimas(100000, 5, "stud_100000.txt");
-    generavimas(1000000, 5, "stud_1000000.txt");
-    generavimas(10000000, 5, "stud_10000000.txt");
+    //Isrikiuoti pagal varda pavarde
+    rikiuoti(studentai_ivedimas, choice);
 
-    Timer timer; // Create a Timer instance
+    // Atvaizduoti studentų duomenis
+    spausdinti(studentai_ivedimas,"ranka_rezultat",choice);
 
-    timer.reset(); // Reset the timer
-    cout << "Laikai su 1000 studentu:" << endl;
+    }
+    else{
 
-    nuskaitytiIsFailo(studentai1, "stud_1000.txt");
-    cout << "nuskaityti truko " << timer.elapsed() << " sekundes" << endl;
+    // Comment out or remove the generavimas lines if not needed
+    // generavimas(1000, 5, "stud_1000.txt");
+    // generavimas(10000, 5, "stud_10000.txt");
+    // generavimas(100000, 5, "stud_100000.txt");
+    // generavimas(1000000, 5, "stud_1000000.txt");
+    // generavimas(10000000, 5, "stud_10000000.txt");
 
-    timer.reset(); // Reset the timer
+    Timer timer;
 
-    isskirstymas(studentai1, studentai1_bahurai, studentai1_slabakai, choice);
-    cout << "isskirstymas truko " << timer.elapsed() << " sekundes" << endl;
+    for (int i = 0; i < 5; ++i) {
+        int numStudents = pow(10, i + 3); // 1000, 10000, 100000, 1000000, 10000000
 
-    timer.reset(); // Reset the timer
+        cout << "Laikai su " << numStudents << " studentu:" << endl;
 
-    rikiuoti(studentai1_bahurai, choice);
-    cout << "rikiuoti bahurus truko " << timer.elapsed() << " sekundes" << endl;
+        timer.reset();
+        nuskaitytiIsFailo(studentai[i], "stud_" + to_string(numStudents) + ".txt");
+        cout << "nuskaityti truko " << timer.elapsed() << " sekundes" << endl;
 
-    timer.reset(); // Reset the timer
+        timer.reset();
+        isskirstymas(studentai[i], bahurai[i], slabakai[i], choice);
+        cout << "isskirstymas truko " << timer.elapsed() << " sekundes" << endl;
 
-    rikiuoti(studentai1_slabakai, choice);
-    cout << "rikiuoti slabakus truko " << timer.elapsed() << " sekundes" << endl;
+        timer.reset();
+        rikiuoti(bahurai[i], choice);
+        cout << "rikiuoti bahurus truko " << timer.elapsed() << " sekundes" << endl;
 
-    timer.reset(); // Reset the timer
+        timer.reset();
+        rikiuoti(slabakai[i], choice);
+        cout << "rikiuoti slabakus truko " << timer.elapsed() << " sekundes" << endl;
 
-    spausdinti(studentai1_bahurai, "results1000_1.txt", choice);
-    cout << "spausdinti bahurus i faila truko " << timer.elapsed() << " sekundes" << endl;
+        timer.reset();
+        spausdinti(bahurai[i], "results" + to_string(numStudents) + "_1.txt", choice);
+        cout << "spausdinti bahurus i faila truko " << timer.elapsed() << " sekundes" << endl;
 
-    timer.reset(); // Reset the timer
-
-    spausdinti(studentai1_slabakai, "results1000_2.txt", choice);
-    cout << "spausdinti slabakus i faila truko " << timer.elapsed() << " sekundes" << endl;
-    timer.reset();
-
-    cout << "Laikai su 10000 studentu:" << endl;
-
-    nuskaitytiIsFailo(studentai1, "stud_10000.txt");
-    cout << "nuskaityti truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    isskirstymas(studentai2, studentai2_bahurai, studentai2_slabakai, choice);
-    cout << "isskirstymas truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    rikiuoti(studentai2_bahurai, choice);
-    cout << "rikiuoti bahurus truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    rikiuoti(studentai2_slabakai, choice);
-    cout << "rikiuoti slabakus truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    spausdinti(studentai2_bahurai, "results10000_1.txt", choice);
-    cout << "spausdinti bahurus i faila truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    spausdinti(studentai2_slabakai, "results10000_2.txt", choice);
-    cout << "spausdinti slabakus i faila truko " << timer.elapsed() << " sekundes" << endl;
-    timer.reset();
-
-    cout << "Laikai su 100000 studentu:" << endl;
-
-    nuskaitytiIsFailo(studentai3, "stud_100000.txt");
-    cout << "nuskaityti truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    isskirstymas(studentai3, studentai3_bahurai, studentai3_slabakai, choice);
-    cout << "isskirstymas truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    rikiuoti(studentai3_bahurai, choice);
-    cout << "rikiuoti bahurus truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    rikiuoti(studentai3_slabakai, choice);
-    cout << "rikiuoti slabakus truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    spausdinti(studentai3_bahurai, "results100000_1.txt", choice);
-    cout << "spausdinti bahurus i faila truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    spausdinti(studentai3_slabakai, "results100000_2.txt", choice);
-    cout << "spausdinti slabakus i faila truko " << timer.elapsed() << " sekundes" << endl;
-    timer.reset();
-
-    cout << "Laikai su 1000000 studentu:" << endl;
-
-    nuskaitytiIsFailo(studentai4, "stud_1000000.txt");
-    cout << "nuskaityti truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    isskirstymas(studentai4, studentai4_bahurai, studentai4_slabakai, choice);
-    cout << "isskirstymas truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    rikiuoti(studentai4_bahurai, choice);
-    cout << "rikiuoti bahurus truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    rikiuoti(studentai4_slabakai, choice);
-    cout << "rikiuoti slabakus truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    spausdinti(studentai4_bahurai, "results1000000_1.txt", choice);
-    cout << "spausdinti bahurus i faila truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    spausdinti(studentai4_slabakai, "results1000000_2.txt", choice);
-    cout << "spausdinti slabakus i faila truko " << timer.elapsed() << " sekundes" << endl;
-    timer.reset();
-
-    cout << "Laikai su 10000000 studentu:" << endl;
-
-    nuskaitytiIsFailo(studentai5, "stud_10000000.txt");
-    cout << "nuskaityti truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    isskirstymas(studentai5, studentai5_bahurai, studentai5_slabakai, choice);
-    cout << "isskirstymas truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    rikiuoti(studentai5_bahurai, choice);
-    cout << "rikiuoti bahurus truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    rikiuoti(studentai5_slabakai, choice);
-    cout << "rikiuoti slabakus truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    spausdinti(studentai5_bahurai, "results10000000_1.txt", choice);
-    cout << "spausdinti bahurus i faila truko " << timer.elapsed() << " sekundes" << endl;
-
-    timer.reset();
-    spausdinti(studentai5_slabakai, "results10000000_2.txt", choice);
-    cout << "spausdinti slabakus i faila truko " << timer.elapsed() << " sekundes" << endl;
-    timer.reset();
-
+        timer.reset();
+        spausdinti(slabakai[i], "results" + to_string(numStudents) + "_2.txt", choice);
+        cout << "spausdinti slabakus i faila truko " << timer.elapsed() << " sekundes" << endl;
+    }
+    }
     return 0;
 }
+
