20fb09ae09718a8c2bdda9b20afc37f5bf82cc5c
jurgisad <144427804+jurgisad@users.noreply.github.com>
2023-12-09 22:32:54 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,49 @@
 #include "header.h"
 #include "timer.h"
 #include "Studentas.h"
+#include <sstream>
 
 int main()
 {
-    //list<Studentas> studentai_ivedimas_l;
-    //vector<Studentas> studentai_ivedimas;
+    vector<Studentas> studentai_ivedimas_s;
+    vector<Studentass> studentai_ivedimas_c;
     char choice;
-    //char choice2;
-    //char choice3;
+    char choice2;
+    char choice3;
+    char choice4;
 
     cout << "Norite naudoti mediana ar vidurki ? (Mediana - a, Vidurkis - b): ";
     ived_tikr_a_or_b(choice);
     cout << "Norite naudoti ivedima ranka ar generuoti failus ? (ranka - a, generuoti - b): ";
     ived_tikr_a_or_b(choice2);
-    //cout << "Norite naudoti sarasus ar vektorius? (sarasus - a, vektorius - b): ";
+    cout << "Norite naudoti klases ar strukturas? (klases - a, strukturas - b): ";
     ived_tikr_a_or_b(choice3);
 
-    /*if(choice3=='a')
+    if(choice3=='a')
     {
         if (choice2=='a')
         {
-            ivestis(studentai_ivedimas_l);
+            choice4='a';
+            while (choice4=='a')
+            {
+                Studentass student;
+                cout << "Iveskti studento informacija (vardas pavarde nd1 nd2 ... egz): ";
+                cin >> student;
+                if (!cin.fail())
+                {
+                    cout << "Informacija issaugota\n" <<  endl;
+                    studentai_ivedimas_c.push_back(student);
+                }
+                else
+                {
+                    std::cerr << "Klaida ivedant studenta" << std::endl;
+                }
 
-
-            rikiuoti(studentai_ivedimas_l, choice);
-
-
-            spausdinti(studentai_ivedimas_l,"ranka_rezultat",choice);
-
+                cout<<"Ar norite ivesti kita studenta? (Taip - a, Ne - b)"<< endl;
+                ived_tikr_a_or_b(choice4);
+            }
+            rikiuoti_(studentai_ivedimas_c, choice);
+            spausdinti_(studentai_ivedimas_c,"ranka_rezultat_c",choice);
         }
         else
         {
@@ -41,12 +56,13 @@ int main()
             // generavimas(10000000, 5, "stud_10000000.txt");
 
             Timer timer;
-            for (int i = 0; i < 5; ++i)
+            for (int i = 0; i < 4; ++i)
             {
                 int numStudents = std::pow(10, i + 3);
-                std::cout << "Laikai su " << numStudents << " studentu:" << std::endl;
-                processStudentData(numStudents, choice);
+                cout << "Laikai su " << numStudents << " studentu:" << endl;
+                processStudentData_class(numStudents, choice);
             }
+
         }
     }
 
@@ -54,18 +70,15 @@ int main()
     {
         if (choice2=='a')
         {
-            ivestis(studentai_ivedimas);
+            ivestis(studentai_ivedimas_s);
 
+            rikiuoti(studentai_ivedimas_s, choice);
 
-            rikiuoti(studentai_ivedimas, choice);
-
-
-            spausdinti(studentai_ivedimas,"ranka_rezultat",choice);
+            spausdinti(studentai_ivedimas_s,"ranka_rezultat_s",choice);
 
         }
         else
         {
-
             // Comment out or remove the generavimas lines if not needed
             // generavimas(1000, 5, "stud_1000.txt");
             // generavimas(10000, 5, "stud_10000.txt");
@@ -75,25 +88,17 @@ int main()
 
             Timer timer;
 
-            for (int i = 0; i < 5; ++i)
+            for (int i = 0; i < 4; ++i)
             {
                 int numStudents = std::pow(10, i + 3);
-                std::cout << "Laikai su " << numStudents << " studentu:" << std::endl;
+                cout << "Laikai su " << numStudents << " studentu:" << endl;
                 processStudentData_vector(numStudents, choice);
             }
+
         }
     }
-    std::cout << "Press Enter to exit...";
-    std::cin.get();
-    return 0;*/
 
-Timer timer;
-            for (int i = 0; i < 5; ++i)
-            {
-                int numStudents = std::pow(10, i + 3);
-                std::cout << "Laikai su " << numStudents << " studentu:" << std::endl;
-                processStudentData_class(numStudents, choice);
-            }
-
+    cout << "Press Enter to exit...";
+    cin.get();
     return 0;
 }
