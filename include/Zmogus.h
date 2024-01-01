d87936526f9c95e618882d33c251724e7b5f9f1b
jurgisad <144427804+jurgisad@users.noreply.github.com>
2024-01-02 01:13:39 +0200

    Update Zmogus.h

--- include/Zmogus.h
+++ include/Zmogus.h
@@ -1,1 +1,31 @@
+#ifndef ZMOGUS_H_INCLUDED
+#define ZMOGUS_H_INCLUDED
 
+#include <string>
+
+class Zmogus
+{
+protected:
+    std::string vardas_;
+    std::string pavarde_;
+
+public:
+    Zmogus(const std::string& vardas, const std::string& pavarde)
+        : vardas_(vardas), pavarde_(pavarde) {}
+
+    Zmogus() : vardas_(""), pavarde_("") {}
+
+    virtual ~Zmogus() {}
+
+    virtual std::string vardas() const
+    {
+        return vardas_;
+    }
+
+    virtual std::string pavarde() const
+    {
+        return pavarde_;
+    }
+};
+
+#endif // ZMOGUS_H_INCLUDED
